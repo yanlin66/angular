ab4c60b05d1a176478ff04ea4344a46a7cdd08ec
codejianqxr <1046396106@qq.com>
2017-01-04 16:18:07 +0800

    costom filter

---
+++ filters.js
@@ -0,0 +1,19 @@
+angular.module("filters",[])
+    .filter("minOrmax",function(){
+        return function(origin,type){
+            var type=type||"min";
+            var temp=origin[0];
+            for(var i=0;i<origin.length;i++){
+               if(type=="min") {
+                   if (origin[i] < temp) {
+                       temp = origin[i]
+                   }
+               }else if(type=="max"){
+                   if (origin[i] > temp){
+                       temp = origin[i]
+                   }
+               }
+            }
+            return temp;
+        }
+    })
